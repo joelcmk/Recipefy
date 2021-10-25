71edc8f22f7d34c8345536c2604433600b615ffd
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-10-25 14:15:54 -0500

    Added nav bar and cleaned code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import RecipeList from './RecipeList';
 import About from './About';
-import Test from './Test';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -33,21 +36,24 @@ const App = () => {
     setValue(recipeName)
   }
 
-
-  console.log(test)
-
-
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </Typography>
+            <Typography variant="h6" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <Link to="/about" className="link">
+                About
+              </Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
         <form onSubmit={handleSubmit}>
           <TextField id="recipeName" label="Recipe name" variant="outlined"
             value={recipeName}
@@ -60,7 +66,7 @@ const App = () => {
 
         <Switch>
           <Route path={`/about`}>
-            <About name={selectedRecipe.name} image={selectedRecipe.img} test={selectedRecipe} />
+            <About />
           </Route>
 
           <Route>
--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -6,6 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import './App.css'
 
 
 const RecipeCard = (props) => {
@@ -25,7 +26,7 @@ const RecipeCard = (props) => {
 
 
   return (
-    <Card sx={{ maxWidth: 345 }} >
+    <Card className="card" sx={{ width: 345 }} >
       <CardMedia
         component="img"
         image={props.information.image}
